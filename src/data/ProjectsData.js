db9a61641e847e96450e6def5304f3dff1904a6d
Aleksey-Kon <windowsviser@gmail.com>
2025-09-30 22:16:38 +0300

    fin

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -13,32 +13,32 @@ export const projectsData = [
     isButton: true,
     isIconButton: true,
     images: [
-      "/images/projects/eva/eva1.webp",
-      "/images/projects/eva/eva2.webp",
-      "/images/projects/eva/eva3.webp",
-      "/images/projects/eva/eva4.webp",
-      "/images/projects/eva/eva5.webp"
+      "images/projects/eva/eva1.webp",
+      "images/projects/eva/eva2.webp",
+      "images/projects/eva/eva3.webp",
+      "images/projects/eva/eva4.webp",
+      "images/projects/eva/eva5.webp"
     ],
-    icon: "/images/icons/steam-black.webp",
+    icon: "images/icons/steam-black.webp",
     href: "https://store.steampowered.com/app/3176450/Eva_Platformer/"
   }),
   new ProjectData({
     isButton: true,
     isIconButton: true,
     images: [
-      "/images/projects/crypt/crypt1.webp",
-      "/images/projects/crypt/crypt2.webp",
+      "images/projects/crypt/crypt1.webp",
+      "images/projects/crypt/crypt2.webp",
     ],
-    icon: "/images/icons/github.webp",
+    icon: "images/icons/github.webp",
     href: "https://github.com/Aleksey-Kon/Aleksey-Kon-Crypt"
   }),
   new ProjectData({
     isButton: false,
     isIconButton: false,
     images: [
-      "/images/projects/spine/1.webp",
-      "/images/projects/spine/2.webp",
-      "/images/projects/spine/3.webp"
+      "images/projects/spine/1.webp",
+      "images/projects/spine/2.webp",
+      "images/projects/spine/3.webp"
     ],
     icon: ""
   }),
@@ -46,8 +46,8 @@ export const projectsData = [
     isButton: false,
     isIconButton: false,
     images: [
-      "/images/projects/site/site1.webp",
-      "/images/projects/site/site2.webp",
+      "images/projects/site/site1.webp",
+      "images/projects/site/site2.webp",
     ],
     icon: ""
   }),
@@ -55,9 +55,9 @@ export const projectsData = [
     isButton: false,
     isIconButton: false,
     images: [
-      "/images/projects/hb-project/1.webp",
-      "/images/projects/hb-project/2.webp",
-      "/images/projects/hb-project/3.webp",
+      "images/projects/hb-project/hb1.webp",
+      "images/projects/hb-project/hb2.webp",
+      "images/projects/hb-project/hb3.webp",
     ],
     icon: ""
   })
--- src/data/ProjectsSectionData.js
+++ src/data/ProjectsSectionData.js
@@ -3,9 +3,9 @@ export class ProjectsSectionData {
     static isIconButton = false;
     //static icon = "/images/icons/steam.webp";
     static images = [
-    "/images/projects/eva/eva1.webp",
-    "/images/projects/crypt/crypt1.webp",
-    "/images/projects/spine/1.webp",
+    "images/projects/eva/eva1.webp",
+    "images/projects/crypt/crypt1.webp",
+    "images/projects/spine/1.webp",
     ];
     static href = "/projects";
 }
